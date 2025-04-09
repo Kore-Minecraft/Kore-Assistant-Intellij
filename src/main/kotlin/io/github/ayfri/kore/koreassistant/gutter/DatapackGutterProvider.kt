e6530e49352a0dcce35d9d9a854f0f0bf1e4bf26
pierre.roy <pierre.roy@eliophot.com>
2025-04-09 14:09:50 +0200

    feat(plugin): Refactor DataPackGutterProvider to use NavigationGutterIconBuilder for line markers.

--- src/main/kotlin/io/github/ayfri/kore/koreassistant/gutter/DatapackGutterProvider.kt
+++ src/main/kotlin/io/github/ayfri/kore/koreassistant/gutter/DatapackGutterProvider.kt
@@ -1,10 +1,16 @@
 package io.github.ayfri.kore.koreassistant.gutter
 
+import com.intellij.codeInsight.daemon.GutterIconNavigationHandler
+import com.intellij.codeInsight.daemon.GutterName
 import com.intellij.codeInsight.daemon.LineMarkerInfo
 import com.intellij.codeInsight.daemon.LineMarkerProvider
+import com.intellij.codeInsight.daemon.LineMarkerProviderDescriptor
+import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder
+import com.intellij.ide.actions.GotoActionAction
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.markup.GutterIconRenderer
 import com.intellij.openapi.util.IconLoader
+import com.intellij.platform.ide.navigation.NavigationService
 import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.source.tree.LeafPsiElement
 import org.jetbrains.kotlin.analysis.api.KaIdeApi
@@ -12,12 +18,14 @@ import org.jetbrains.kotlin.analysis.api.analyze
 import org.jetbrains.kotlin.analysis.api.resolution.successfulFunctionCallOrNull
 import org.jetbrains.kotlin.analysis.api.resolution.symbol
 import org.jetbrains.kotlin.analysis.api.symbols.name
+import org.jetbrains.kotlin.codegen.StackValue.expression
 import org.jetbrains.kotlin.idea.references.mainReference
 import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.psi.KtNameReferenceExpression
+import javax.swing.Icon
 
 data object KoreNames {
 	val KORE_DATAPACK_CLASS_ID = FqName("io.github.ayfri.kore.dataPack")
@@ -29,12 +37,25 @@ data object Icons {
 	val ICON = IconLoader.getIcon("/images/kore-white.svg", DataPackGutterProvider::class.java.classLoader)
 }
 
-class DataPackGutterProvider : LineMarkerProvider {
+class ActionnableLineMarkerInfo<T : PsiElement>(
+	element: T,
+	icon: Icon,
+	tooltip: ((T) -> String)?,
+	name: () -> String,
+	alignement: GutterIconRenderer.Alignment = GutterIconRenderer.Alignment.RIGHT,
+	onClick: GutterIconNavigationHandler<T>,
+) : LineMarkerInfo<T>(
+	element,
+	element.textRange,
+	icon,
+	tooltip,
+	onClick,
+	alignement,
+	name
+)
 
-	companion object {
-
-		private val LOGGER = Logger.getInstance(DataPackGutterProvider::class.java)
-	}
+class DataPackGutterProvider : LineMarkerProviderDescriptor() {
+	override fun getName() = "DatapackGutterProvider"
 
 	@OptIn(KaIdeApi::class)
 	override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<*>? {
@@ -60,8 +81,6 @@ class DataPackGutterProvider : LineMarkerProvider {
 		// Use the Analysis API to resolve the call
 		analyze(callExpression) { // 'this' is the KaSession
 			// Use resolveToCall and get the symbol
-			LOGGER.info("Resolving function call: ${callExpression.resolveToCall()?.successfulFunctionCallOrNull()?.symbol}")
-			LOGGER.info("Resolving function call: ${callExpression.mainReference.resolveToSymbol()} ${callExpression.mainReference.resolveToSymbol()?.importableFqName}")
 			val functionCall = callExpression.resolveToCall()?.successfulFunctionCallOrNull() ?: return null
 			// Ensure it's the correct symbol type
 			val functionSymbol = functionCall.symbol
@@ -72,15 +91,13 @@ class DataPackGutterProvider : LineMarkerProvider {
 			if (functionSymbol.callableId?.asSingleFqName() == KoreNames.KORE_DATAPACK_CLASS_ID &&
 			    functionSymbol.name == KoreNames.KORE_DATAPACK_NAME) {
 				// Found the correct function, create the marker
-				return LineMarkerInfo(
-					element, // Anchor to the identifier PsiElement
-					element.textRange,
-					Icons.ICON,
-					{ "Kore dataPack definition" }, // Tooltip text
-					null, // Navigation handler (none for now)
-					GutterIconRenderer.Alignment.CENTER,
-					{ "Kore dataPack" } // Accessibility name
-				)
+				val iconBuilder = NavigationGutterIconBuilder.create(Icons.ICON)
+					.setAlignment(GutterIconRenderer.Alignment.CENTER)
+					.setTooltipTitle("DataPack Definition")
+					.setTooltipText("Kore dataPack definition")
+					.setTarget(functionCall.symbol.valueParameters.first().psi)
+					.createLineMarkerInfo(element)
+				return iconBuilder
 			}
 		}
 
@@ -92,4 +109,8 @@ class DataPackGutterProvider : LineMarkerProvider {
 		elements: MutableList<out PsiElement>,
 		result: MutableCollection<in LineMarkerInfo<*>>
 	) {}
+
+	companion object {
+		private val LOGGER = Logger.getInstance(DataPackGutterProvider::class.java)
+	}
 }
