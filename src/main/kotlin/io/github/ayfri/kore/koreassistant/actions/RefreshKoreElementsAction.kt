bd946df796bb03414a790323f3805cac6973003a
Ayfri <pierre.ayfri@gmail.com>
2026-07-22 09:34:01 +0200

    fix(actions): specify BGT action update thread for refresh action

--- src/main/kotlin/io/github/ayfri/kore/koreassistant/actions/RefreshKoreElementsAction.kt
+++ src/main/kotlin/io/github/ayfri/kore/koreassistant/actions/RefreshKoreElementsAction.kt
@@ -1,6 +1,7 @@
 package io.github.ayfri.kore.koreassistant.actions
 
 import com.intellij.icons.AllIcons
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 
@@ -11,4 +12,6 @@ data class RefreshKoreElementsAction(private val refreshCallback: () -> Unit) :
 	AnAction("Refresh", "Reload Kore elements list", AllIcons.Actions.Refresh) {
 
 	override fun actionPerformed(e: AnActionEvent) = refreshCallback()
+
+	override fun getActionUpdateThread() = ActionUpdateThread.BGT
 }
