bcd0c81f584fb348b742c3a90417cedb9482dc92
Ayfri <pierre.ayfri@gmail.com>
2026-07-27 03:20:16 +0200

    test(index): Cover reordered arguments, in-file constants and interpolated names.

--- src/test/kotlin/io/github/ayfri/kore/koreassistant/index/KoreDeclarationIndexTest.kt
+++ src/test/kotlin/io/github/ayfri/kore/koreassistant/index/KoreDeclarationIndexTest.kt
@@ -6,6 +6,8 @@ import com.intellij.testFramework.fixtures.BasePlatformTestCase
 private val MAIN_KT = """
 	import io.github.ayfri.kore.dataPack
 
+	const val PREFIX = "generated/"
+
 	fun main() {
 		dataPack("rotten-flesh-to-leather") {
 			recipes {
@@ -16,9 +18,18 @@ private val MAIN_KT = """
 
 			function("helper", namespace = "other", directory = "utils") { }
 			function("positional", "ns2", "dir2") { }
+			function(directory = "reordered_dir", name = "reordered") { }
 
 			advancement("root") {
 				namespace = "adv_ns"
+			}
+
+			predicate(fileName = PREFIX + "concatenated") { }
+
+			listOf("oak", "birch").forEach { leaf ->
+				lootTable("blocks/${'$'}leaf") {
+					namespace = "minecraft"
+				}
 			}
 		}
 	}
@@ -35,7 +46,16 @@ class KoreDeclarationIndexTest : BasePlatformTestCase() {
 		val indexed = index()
 
 		assertEquals(
-			setOf("rotten-flesh-to-leather", "browndye_smelt", "helper", "positional", "root"),
+			setOf(
+				"rotten-flesh-to-leather",
+				"browndye_smelt",
+				"helper",
+				"positional",
+				"reordered",
+				"root",
+				"generated/concatenated",
+				"blocks/\$leaf",
+			),
 			indexed.keys,
 		)
 		assertEquals(KoreDeclarationKind.DATA_PACK, indexed.getValue("rotten-flesh-to-leather").kind)
@@ -72,5 +92,28 @@ class KoreDeclarationIndexTest : BasePlatformTestCase() {
 
 	fun testLeavesNamespaceUnsetWhenTheDeclarationDoesNotSpellItOut() {
 		assertNull(index().getValue("browndye_smelt").namespace)
+	}
+
+	fun testReadsTheNameFromAReorderedNamedArgument() {
+		val reordered = index().getValue("reordered")
+
+		assertEquals(KoreDeclarationKind.FUNCTION, reordered.kind)
+		assertEquals("reordered_dir", reordered.directory)
+	}
+
+	fun testFollowsConstantsAndConcatenationDeclaredInTheSameFile() {
+		val concatenated = index().getValue("generated/concatenated")
+
+		assertEquals(KoreDeclarationKind.PREDICATE, concatenated.kind)
+		assertFalse("A fully resolved name must not be marked dynamic", concatenated.isDynamic)
+	}
+
+	fun testKeepsInterpolatedNamesAsTemplates() {
+		val inLoop = index().getValue("blocks/\$leaf")
+
+		assertEquals(KoreDeclarationKind.LOOT_TABLE, inLoop.kind)
+		assertEquals("minecraft", inLoop.namespace)
+		assertTrue("An interpolated name is only known at runtime", inLoop.isDynamic)
+		assertEquals("rotten-flesh-to-leather", inLoop.dataPackName)
 	}
 }
